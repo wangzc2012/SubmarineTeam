af5fa8a4a0bc5c8b061c9da7c6b11d215bc9227a
wangzc2012 <1221613@qq.com>
2024-12-07 18:26:52 +0800

    test

--- docs/.vitepress/config.mjs
+++ docs/.vitepress/config.mjs
@@ -13,7 +13,25 @@ export default defineConfig({
     socialLinks: [
       { icon: 'github', link: 'https://github.com/wangzc2012/SubmarineTeam' }
     ],
-
+    search: {
+      provider:"local",
+      options: {
+        translations: {
+          button:{
+            buttonText: "搜索文档",
+            buttonAriaLabel:"搜索文档",
+          },
+          modal: {
+            noResultsText:"无法找到相关结果",
+            resetButtonTitle:"清除查询条件",
+            footer: {
+              selectText:"选择",
+              navigateText:"切换",
+            },
+          },
+        },
+      },
+    },
     footer: {
       copyright: "此网站由 wangzc2012 开发，TzzlStudio 大力支持",
     },
