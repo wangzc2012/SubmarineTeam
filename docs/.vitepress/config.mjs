411aad81ab9325707fda6404e9f3b4352f998fac
wangzc2012 <1221613@qq.com>
2024-12-07 17:58:34 +0800

    test

--- docs/.vitepress/config.mjs
+++ docs/.vitepress/config.mjs
@@ -11,7 +11,7 @@ export default defineConfig({
     ],
 
     socialLinks: [
-      { icon: 'github', link: 'https://github.com/vuejs/vitepress' }
+      { icon: 'github', link: 'https://github.com/wangzc2012/SubmarineTeam' }
     ],
 
     footer: {
